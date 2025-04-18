4416f8a92387e8e81cd7bf4efe2b12e4ee07e5fa
foxynhoz <waterfox24699@gmail.com>
2025-04-18 02:39:19 -0300

    Removed Minimaps

--- STOOPID ROOM/Main_Game.c
+++ STOOPID ROOM/Main_Game.c
@@ -35,25 +35,11 @@ if (language == 2) {
 while (gameOver == false) {
     //MAIN LOOP
     while (room_side == 1) {
-        printf("\n╔══╝---╚══◙◙════╗\n"
-                "║                 ║\n"
-                "║⑊              〢║\n"
-                "║⑊      ▲       〢║\n"
-                "║⑊              〢║\n"
-                "║                 ║\n"
-                "╚════𝄃𝄃𝄀𝄃════╝\n");
         printf("\nOlhando em volta, voce ve uma porta com um cofre ao lado\nO que voce faz?\n\n1 - Inspeciona a porta\n2 - Inspeciona o cofre\n3 - Virar para a esquerda\n4 - Virar para a direita\n\n>> ");
         scanf("%d", &choice);
 
         //DOOR ROUTE
         if (choice == 1) {
-            printf("\n╔══╝---╚══◙◙═══════╗\n"
-                     "║    ▲               ║\n"
-                     "║⑊                 〢║\n"
-                     "║⑊                 〢║\n"
-                     "║⑊                 〢║\n"
-                     "║                    ║\n"
-                     "╚════𝄃𝄃𝄀𝄂𝄃═════╝\n");
             printf("\nVoce inspeciona a porta de ferro, ela parece estar trancada por uma chave\nO que voce faz?\n\n1 - Abrir cadeado\n2 - Voltar\n\n>> ");
             scanf("%d", &choice);
             if (choice == 1 && hasKey == true) {
@@ -75,32 +61,13 @@ while (gameOver == false) {
 
         //VAULT ROUTE
         if (choice == 2) {
-            printf("\n╔══╝---╚══◙◙═══════╗\n"
-                     "║          ▲         ║\n"
-                     "║⑊                 〢║\n"
-                     "║⑊                 〢║\n"
-                     "║⑊                 〢║\n"
-                     "║                    ║\n"
-                     "╚════𝄃𝄃𝄀𝄂𝄃═════╝\n");
             sleep(2);
-            printf("\n╔════════════════════════╗\n"
-                     "║╭━━━╮╭━━━╮╭━━━╮║  ║\n"
-                     "║┃╭━╮┃┃╭━╮┃┃╭━╮┃║  ║\n"
-                     "║┃╰━╯┃┃╰━╯┃┃╰━╯┃║  ║\n"
-                     "║╰━━━╯╰━━━╯╰━━━╯║  ║\n"
-                     "╚════════════════════════╝\n");
             if (isVaultOpen == false) {
                 printf("\nVoce inspeciona o cofre... ele pede um codigo de 3 digitos\nDigite o codigo: ");
                 scanf("%d", &code);
                 if (code == 420) {
                     hasKey = true;
                     isVaultOpen = true;
-                    printf("\nooo,    .---.\n"
-                        " o` o  /     |_________________\n"
-                        "o`'oooo  ()    ________   _   _)\n"
-                        "`ooo`  |     |/        | | |_|\n"
-                        "`ooo'   `---'         "" |_|\n"
-                        "\n");
                     sleep(2);
                     printf("\nO cofre se abre, dentro dele ha uma chave\nVoce pega a chave e volta ao centro da sala onde\n");
                     break;
@@ -126,24 +93,10 @@ while (gameOver == false) {
     }
     //PAINTING SIDE
     while (room_side == 2) {
-        printf("\n╔══╝---╚══◙◙════╗\n"
-                "║                  ║\n"
-                "║⑊               〢║\n"
-                "║⑊      ▶        〢║\n"
-                "║⑊               〢║\n"
-                "║                  ║\n"
-                "╚════𝄃𝄃𝄀══════╝\n");
         printf("Voce ve uma pintura de uma criatura amarela na parede\nO que voce faz?\n\n1 - Inspecionar a pintura\n2 - Virar a esquerda\n3 - Virar a direita\n\n>> ");
         scanf("%d", &choice);
         //PAINTING ROUTE
         if (choice == 1)
-            printf("\n╔══╝---╚══◙◙════╗\n"
-                     "║                 ║\n"
-                     "║⑊              〢║\n"
-                     "║⑊            ▶ 〢║\n"
-                     "║⑊              〢║\n"
-                     "║                 ║\n"
-                     "╚════𝄃𝄃𝄃═════╝\n");
             printf("Voce inspeciona a pintura, e atraz dela, voce encontra um texto enigmatico que diz:\n\nhttps://imgur.com/a/bSdx0Y2\n\nVoce nao sabe o que isso significa, mas reconhece onde poderia ser util ;)\nVoce volta ao centro da sala e\n");
         //SIDE CHANGE
         if (choice == 2) {
@@ -157,13 +110,6 @@ while (gameOver == false) {
     while (room_side == 3) {
         //WINDOW ROUTE
         if (isWindowOpen == false) {
-            printf("\n╔══╝---╚══◙◙════╗\n"
-                    "║                  ║\n"
-                    "║⑊               〢║\n"
-                    "║⑊      ▼        〢║\n"
-                    "║⑊               〢║\n"
-                    "║                  ║\n"
-                    "╚════𝄃𝄃𝄀══════╝\n");
             printf("Voce ve uma janela fechada por uma cortina\nO que voce faz?\n\n1 - Abrir a cortina\n2 - Virar a esquerda\n3 - Virar a direita\n\n>> ");
             scanf("%d", &choice);
             if (choice == 1) {
@@ -172,13 +118,6 @@ while (gameOver == false) {
             }
         }
         if (isWindowOpen == true) {
-            printf("\n╔══╝---╚══◙◙════╗\n"
-                    "║                  ║\n"
-                    "║⑊               〢║\n"
-                    "║⑊      ▼        〢║\n"
-                    "║⑊               〢║\n"
-                    "║                  ║\n"
-                    "╚════𝄃𝄃𝄀══════╝\n");
             printf("Voce ve uma janela com grades, e no fundo, uma arvore \nO que voce faz?\n\n1 - Inspecionar la fora\n2 - Virar a esquerda\n3 - Virar a direita\n\n>> ");
             scanf("%d", &choice);
             }
@@ -200,25 +139,11 @@ while (gameOver == false) {
     }
     //BLOOD SIDE
     while (room_side == 4) {
-        printf("\n╔══╝---╚══◙◙════╗\n"
-                 "║                 ║\n"
-                 "║⑊              〢║\n"
-                 "║⑊      ◀       〢║\n"
-                 "║⑊              〢║\n"
-                 "║                 ║\n"
-                 "╚════𝄃𝄃𝄀═════╝\n");
         printf("Voce ve uma manchas escuras enormes na parede\nO que voce faz?\n\n1 - Inspecionar as manchas\n2 - Virar a esquerda\n3 - Virar a direita\n\n>> ");
         scanf("%d", &choice);
         //BLOOD ROUTE
         if (choice == 1) {
             if (isWindowOpen == false) {
-                printf("\n╔══╝---╚══◙◙════╗\n"
-                 "║                 ║\n"
-                 "║⑊              〢║\n"
-                 "║⑊ ◀            〢║\n"
-                 "║⑊              〢║\n"
-                 "║                 ║\n"
-                 "╚════𝄃𝄃𝄀═════╝\n");
                 printf("Voce inspeciona as manchas, porem\nestá muito escuro para ver direito o que elas são\nO que voce faz?\n\n1 - Voltar\n\n>> ");
                 scanf("%d", &choice);
                 if (choice == 1) {
@@ -227,13 +152,6 @@ while (gameOver == false) {
             }
         if (choice == 1) {
             if (isWindowOpen == true) {
-                printf("\n╔══╝---╚══◙◙════╗\n"
-                 "║                 ║\n"
-                 "║⑊              〢║\n"
-                 "║⑊ ◀            〢║\n"
-                 "║⑊              〢║\n"
-                 "║                 ║\n"
-                 "╚════𝄃𝄃𝄀═════╝\n");
                 printf("Voce inspeciona as manchas\nForam pintadas ali com sangue\nmas voce consegue claramente distinguir\ndois caracteres escritos\n\nA4\n\nO que voce faz?\n\n1 - Voltar\n\n>> ");
                     scanf("%d", &choice);
                     if (choice == 1) {
@@ -264,25 +182,11 @@ if (language == 1) {
     while (gameOver == false) {
         //MAIN LOOP
         while (room_side == 1) {
-            printf("\n╔══╝---╚══◙◙════╗\n"
-                "║                 ║\n"
-                "║⑊              〢║\n"
-                "║⑊      ▲       〢║\n"
-                "║⑊              〢║\n"
-                "║                 ║\n"
-                "╚════𝄃𝄃𝄀𝄃════╝\n");
             printf("\nLooking around you see a door, with a vault by its side\nWhat do you do?\n\n1 - Inspect the door\n2 - Inspect the vault\n3 - Turn Left\n4 - Turn Right\n\n>> ");
             scanf("%d", &choice);
 
             //DOOR ROUTE
             if (choice == 1) {
-                printf("\n╔══╝---╚══◙◙═══════╗\n"
-                     "║    ▲               ║\n"
-                     "║⑊                 〢║\n"
-                     "║⑊                 〢║\n"
-                     "║⑊                 〢║\n"
-                     "║                    ║\n"
-                     "╚════𝄃𝄃𝄀𝄂𝄃═════╝\n");
                 printf("\nYou inspect the heavy iron door, it's locked with a padlock\nWhat do you do?\n\n1 - Open Padlock\n2 - Go back\n\n>> ");
                 scanf("%d", &choice);
                 if (choice == 1 && hasKey == true) {
@@ -304,32 +208,13 @@ if (language == 1) {
 
             //VAULT ROUTE
             if (choice == 2) {
-                printf("\n╔══╝---╚══◙◙═══════╗\n"
-                     "║          ▲         ║\n"
-                     "║⑊                 〢║\n"
-                     "║⑊                 〢║\n"
-                     "║⑊                 〢║\n"
-                     "║                    ║\n"
-                     "╚════𝄃𝄃𝄀𝄂𝄃═════╝\n");
                 sleep(2);
-                printf("\n╔════════════════════════╗\n"
-                     "║╭━━━╮╭━━━╮╭━━━╮║  ║\n"
-                     "║┃╭━╮┃┃╭━╮┃┃╭━╮┃║  ║\n"
-                     "║┃╰━╯┃┃╰━╯┃┃╰━╯┃║  ║\n"
-                     "║╰━━━╯╰━━━╯╰━━━╯║  ║\n"
-                     "╚════════════════════════╝\n");;
                 if (isVaultOpen == false) {
                     printf("\nYou inspect the vault... it asks for a 3-digit code\nEnter the code: ");
                     scanf("%d", &code);
                     if (code == 420) {
                         hasKey = true;
                         isVaultOpen = true;
-                        printf("\nooo,    .---.\n"
-                            " o` o  /     |_________________\n"
-                            "o`'oooo  ()    ________   _   _)\n"
-                            "`ooo`  |     |/        | | |_|\n"
-                            "`ooo'   `---'         "" |_|\n"
-                            "\n");
                         sleep(2);
                         printf("\nThe vault opens, inside it there's a key\nYou take the key and return to the center of the room where\n");
                         break;
@@ -355,24 +240,10 @@ if (language == 1) {
         }
         //PAINTING SIDE
         while (room_side == 2) {
-            printf("\n╔══╝---╚══◙◙════╗\n"
-                "║                  ║\n"
-                "║⑊               〢║\n"
-                "║⑊      ▶        〢║\n"
-                "║⑊               〢║\n"
-                "║                  ║\n"
-                "╚════𝄃𝄃𝄀══════╝\n");
             printf("You see a painting of a yellow round creature on the wall\nWhat do you do?\n\n1 - Inspect the painting\n2 - Turn left\n3 - Turn right\n\n>> ");
             scanf("%d", &choice);
             //PAINTING ROUTE
             if (choice == 1)
-                printf("\n╔══╝---╚══◙◙════╗\n"
-                     "║                 ║\n"
-                     "║⑊              〢║\n"
-                     "║⑊            ▶ 〢║\n"
-                     "║⑊              〢║\n"
-                     "║                 ║\n"
-                     "╚════𝄃𝄃𝄃═════╝\n");
                 printf("You inspect the painting, and behind it, you find an enigmatic text that says:\n\nhttps://imgur.com/a/bSdx0Y2\n\nYou don't know what this means, but you recognize where it might be useful ;)\nYou return to the center of the room and\n");
             //SIDE CHANGE
             if (choice == 2) {
@@ -386,13 +257,6 @@ if (language == 1) {
         while (room_side == 3) {
             //WINDOW ROUTE
             if (isWindowOpen == false) {
-                printf("\n╔══╝---╚══◙◙════╗\n"
-                    "║                  ║\n"
-                    "║⑊               〢║\n"
-                    "║⑊      ▼        〢║\n"
-                    "║⑊               〢║\n"
-                    "║                  ║\n"
-                    "╚════𝄃𝄃𝄀══════╝\n");
                 printf("You see a window covered by a curtain\nWhat do you do?\n\n1 - Open the curtain\n2 - Turn left\n3 - Turn right\n\n>> ");
                 scanf("%d", &choice);
                 if (choice == 1) {
@@ -401,13 +265,6 @@ if (language == 1) {
                 }
             }
             if (isWindowOpen == true) {
-                printf("\n╔══╝---╚══◙◙════╗\n"
-                    "║                  ║\n"
-                    "║⑊               〢║\n"
-                    "║⑊      ▼        〢║\n"
-                    "║⑊               〢║\n"
-                    "║                  ║\n"
-                    "╚════𝄃𝄃𝄀══════╝\n");
                 printf("You see a window with bars, and in the distance, a tree\nWhat do you do?\n\n1 - Inspect outside\n2 - Turn left\n3 - Turn right\n\n>> ");
                 scanf("%d", &choice);
                 }
@@ -429,25 +286,11 @@ if (language == 1) {
         }
         //BLOOD SIDE
         while (room_side == 4) {
-            printf("\n╔══╝---╚══◙◙════╗\n"
-                 "║                 ║\n"
-                 "║⑊              〢║\n"
-                 "║⑊      ◀       〢║\n"
-                 "║⑊              〢║\n"
-                 "║                 ║\n"
-                 "╚════𝄃𝄃𝄀═════╝\n");
             printf("You see huge dark stains on the wall\nWhat do you do?\n\n1 - Inspect the stains\n2 - Turn left\n3 - Turn right\n\n>> ");
             scanf("%d", &choice);
             //BLOOD ROUTE
             if (choice == 1) {
                 if (isWindowOpen == false) {
-                    printf("\n╔══╝---╚══◙◙════╗\n"
-                 "║                 ║\n"
-                 "║⑊              〢║\n"
-                 "║⑊ ◀            〢║\n"
-                 "║⑊              〢║\n"
-                 "║                 ║\n"
-                 "╚════𝄃𝄃𝄀═════╝\n");
                     printf("You inspect the stains, but\nit's too dark to clearly see what they are\nWhat do you do?\n\n1 - Go back\n\n>> ");
                     scanf("%d", &choice);
                     if (choice == 1) {
