a74386be6af8c2c9149b5c5f5e766589684840d6
foxynhoz <waterfox24699@gmail.com>
2025-04-23 16:46:01 -0300

    Updates

---
+++ funçoes/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int roboMatematico(int a, int b);
+
+
+int main(){
+
+    int numero_1 = 5;
+    int numero_2 = 7;
+
+    roboMatematico(numero_1, numero_2);
+
+
+}
+
+int roboMatematico(int a,int b){
+
+    return a+b;
+
+}
